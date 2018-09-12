91847c2adc4a7e878f32189f70f2ce0631214268
vazz <elisa.merinovazquez@gmail.com>
2018-09-12 12:01:59 +0200

    so far so food

--- src/js/FoodBox.jsx
+++ src/js/FoodBox.jsx
@@ -1,38 +1,45 @@
-import React from 'react'
+import React from 'react';
 
 const FoodBox = ({ food, handleQtyChange }) => {
-    return (
-        <div className="box">
-            <article className="media">
-                <div className="media-left">
-                    <figure className="image is-64x64">
-                        <img src={food.image} />
-                    </figure>
-                </div>
-                <div className="media-content">
-                    <div className="content">
-                        <p>
-                            <strong>{food.name}</strong> <br />
-                            <small>{food.calories} cal</small>
-                        </p>
-                    </div>
-                </div>
-                <div className="media-right">
-                    <div className="field has-addons">
-                        <div className="control">
-                            <input
-                                min="0"
-                                className="input"
-                                type="number"
-                                onChange={evt => handleQtyChange(food.name, evt.target.value)}
-                                value={food.quantity}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </article>
-        </div>
-    )
-}
+	const ratio = food.calories / 750;
 
-export default FoodBox
+	const red = 50 + parseInt(ratio * 255);
+	const green = 250 - parseInt(ratio * 255);
+
+	console.log(red, +', ' + green);
+
+	return (
+		<div className="box" style={{ backgroundColor: `rgba(${red},${green},15,0.5` }}>
+			<article className="media">
+				<div className="media-left">
+					<figure className="image is-64x64">
+						<img src={food.image} />
+					</figure>
+				</div>
+				<div className="media-content">
+					<div className="content">
+						<p>
+							<strong>{food.name}</strong> <br />
+							<small>{food.calories} cal</small>
+						</p>
+					</div>
+				</div>
+				<div className="media-right">
+					<div className="field has-addons">
+						<div className="control">
+							<input
+								min="0"
+								className="input"
+								type="number"
+								onChange={(evt) => handleQtyChange(food.name, evt.target.value)}
+								value={food.quantity}
+							/>
+						</div>
+					</div>
+				</div>
+			</article>
+		</div>
+	);
+};
+
+export default FoodBox;
--- src/js/Today.jsx
+++ src/js/Today.jsx
@@ -1,33 +1,41 @@
-import React from 'react'
+import React from 'react';
 
 const Today = ({ foods }) => {
-    const qtyFood = foods
-        .map(el => {
-            return { ...el, quantity: parseInt(el.quantity) }
-        })
-        .filter(el => el.quantity)
+	const qtyFood = foods
+		.map((el) => {
+			return { ...el, quantity: parseInt(el.quantity) };
+		})
+		.filter((el) => el.quantity);
 
-    const mappedFoods = qtyFood.map((el, index) => {
-        return (
-            <li key={index}>
-                {el.quantity} {el.name}
-                {el.quantity > 1 && el.name[el.name.length - 1] !== 's' ? 's' : ''} ={' '}
-                {el.quantity * el.calories} cal
-            </li>
-        )
-    })
+	const mappedFoods = qtyFood.map((el, index) => {
+		return (
+			<li key={index}>
+				{el.quantity} {el.name}
+				{el.quantity > 1 && el.name[el.name.length - 1] !== 's' ? 's' : ''} ={' '}
+				{el.quantity * el.calories} cal
+			</li>
+		);
+	});
 
-    const totalCalories = qtyFood.reduce((acc, val) => {
-        return acc + val.calories * val.quantity
-    }, 0)
+	const totalCalories = qtyFood.reduce((acc, val) => {
+		return acc + val.calories * val.quantity;
+	}, 0);
+	let message = '';
+	if (totalCalories < 200) {
+		message = 'get some more foood!';
+	} else {
+		message = 'its all good today';
+	}
+	//Calories;
+	return (
+		<div className="today">
+			<h2>Today's Menu</h2>
+			<ul>{mappedFoods}</ul>
+			<strong>Total calories: {totalCalories} cal</strong>
+			<br />
+			<strong>{message}</strong>
+		</div>
+	);
+};
 
-    return (
-        <div className="today">
-            <h2>Today's Menu</h2>
-            <ul>{mappedFoods}</ul>
-            <strong>Total calories: {totalCalories} cal</strong>
-        </div>
-    )
-}
-
-export default Today
+export default Today;
